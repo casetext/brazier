8c194ea3c27ca48a159251554548d5e2b4bad25d
Harry Schmidt <goldibex@casetext.com>
2015-06-22 09:38:07 -0700

    add specs for mv

--- spec/lib/mv_spec.js
+++ spec/lib/mv_spec.js
@@ -1,20 +1,68 @@
 
 'use strict';
 
+var Firebase = require('firebase');
+var mv = require('../../lib/mv');
+
+
 describe('mv', function() {
 
-  var mv = require('../../lib/mv');
+  var root;
 
-  it('moves data recursively from one ref to another', function() {
+  beforeEach(function(done) {
+
+    root = new Firebase('brazier-mv-spec.firebaseio-demo.com');
+    root.set({
+      source: {
+        a: 1,
+        b: ['foo'],
+        c: {
+          bells: true,
+          whistles: true
+        }
+      },
+      target: {
+        a: 2,
+        b: ['bar', 'baz', 'quux'],
+        c: {
+          bells: false
+        }
+      }
+    }, done);
 
   });
 
-  it('fails if data cannot be recursively merged', function() {
+  it('moves data recursively from one ref to another', function(done) {
+
+    mv(root.child('source'), root.child('target'), function(err) {
+
+      expect(err).toBe(null);
+
+      root.child('target')
+      .once('value', function(snap) {
+
+        expect(snap.val()).toEqual({
+          a: 1,
+          b: ['foo', 'baz', 'quux'],
+          c: {
+            bells: true,
+            whistles: true
+          }
+        });
+
+        root.child('source')
+        .once('value', function(snap) {
+          expect(snap.val()).toBe(null);
+          done();
+        }, done);
+
+      }, done);
+
+    });
 
   });
 
-  it('does not delete the source if the copy phase fails', function() {
-
-  });
-
+  // TODO(goldibex): use a real Firebase so we can test the behavior of mv on failure
+  // i.e., that it doesn't delete existing data if the copy failed
+  
 });
